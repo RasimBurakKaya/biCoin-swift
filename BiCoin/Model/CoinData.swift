97680f6bc0ac09360961491b4096ca7fa627979e
Rasim Burak Kaya <kayarasimburak@gmail.com>
2024-04-17 18:57:28 +0300

    Last Commit

---
+++ BiCoin/Model/CoinData.swift
@@ -0,0 +1,8 @@
+//
+//  CoinData.swift
+//  BiCoin
+//
+//  Created by Rasim Burak Kaya on 17.04.2024.
+//
+
+import Foundation
---
+++ BiCoin/Model/CoinManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoinManager.swift
+//  BiCoin
+//
+//  Created by Rasim Burak Kaya on 17.04.2024.
+//
+
+import Foundation
